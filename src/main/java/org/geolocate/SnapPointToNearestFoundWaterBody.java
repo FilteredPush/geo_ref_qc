e9fa2cb7c5320cc57339685b70f233f33ca19106
chicoreus <mole@morris.net>
2022-02-15 13:47:40 -0500

    ISSUE: Redmine 868 PURPOSE: Update generated geolocate client.  DESCRIPTION: Adding classes generated from moved and updated geolocate WSDL soap service definition at https://www.geo-locate.org/webservices/geolocatesvcv2/geolocatesvc.asmx?WSDL using code generated with https://github.com/FilteredPush/gen_geolocate_client

---
+++ src/main/java/org/geolocate/SnapPointToNearestFoundWaterBody.java
@@ -0,0 +1,92 @@
+
+package org.geolocate;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="LocalityDescription" type="{http://geo-locate.org/webservices/}LocalityDescription"/&gt;
+ *         &lt;element name="WGS84Coordinate" type="{http://geo-locate.org/webservices/}GeographicPoint"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "localityDescription",
+    "wgs84Coordinate"
+})
+@XmlRootElement(name = "SnapPointToNearestFoundWaterBody")
+public class SnapPointToNearestFoundWaterBody {
+
+    @XmlElement(name = "LocalityDescription", required = true)
+    protected LocalityDescription localityDescription;
+    @XmlElement(name = "WGS84Coordinate", required = true)
+    protected GeographicPoint wgs84Coordinate;
+
+    /**
+     * Gets the value of the localityDescription property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link LocalityDescription }
+     *     
+     */
+    public LocalityDescription getLocalityDescription() {
+        return localityDescription;
+    }
+
+    /**
+     * Sets the value of the localityDescription property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link LocalityDescription }
+     *     
+     */
+    public void setLocalityDescription(LocalityDescription value) {
+        this.localityDescription = value;
+    }
+
+    /**
+     * Gets the value of the wgs84Coordinate property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link GeographicPoint }
+     *     
+     */
+    public GeographicPoint getWGS84Coordinate() {
+        return wgs84Coordinate;
+    }
+
+    /**
+     * Sets the value of the wgs84Coordinate property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link GeographicPoint }
+     *     
+     */
+    public void setWGS84Coordinate(GeographicPoint value) {
+        this.wgs84Coordinate = value;
+    }
+
+}
